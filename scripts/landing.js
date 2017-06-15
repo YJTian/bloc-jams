51faa63c97a812e3acb2dadf80b03b6dd5755692
YijieTian <tianyijie@msn.com>
2017-06-15 17:01:14 -0500

    assignment 22 events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
+//move the function revealPoint out to global, and an array as the argument.
+var revealPoint = function (arrPoints){
+  arrPoints.style.opacity = 1;
+  arrPoints.style.transform = "scaleX(1) translateY(0)";
+  arrPoints.style.msTransform = "scaleX(1) translateY(0)";
+  arrPoints.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 var animatePoints = function(points) {
-
-  var revealPoint = function (index){
-
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  for (var i = 0; i < points.length; i++) {
-    revealPoint (i);
-  };
+  /*add forEachFunction function which takes an array and a callback function
+  revealPoint as arguments, and get rid of the previous "for" loop*/
+  forEachFunction(points, revealPoint);
 };
 
 window.onload = function() {
